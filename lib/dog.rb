e4346605bb44f65821d6d9b92b6096a46855e604
Justin Movitz <movitzj1@montclair.edu>
2021-01-28 02:01:56 -0500

    commit tree f258eb80b9d29d04b44f91cfaceea992c2ff7352

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
 
-    def name=(dogs_name)
-      @this_dogs_name = dogs_name
-    end
-  
-    def name
-      @this_dogs_name
-    end
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
   
-  lassie = Dog.new
-  lassie.name = "Lassie"
+  def name
+    @this_dogs_name
+  end
+end
   
-  puts lassie.name
+lassie = Dog.new
+lassie.name = "Lassie"
+  
+puts lassie.name
